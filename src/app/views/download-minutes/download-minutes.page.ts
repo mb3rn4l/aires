d494a1ca011285c9df82df3a4cd0943c62d779d5
Miguel Bernal <miguel.bernal.colonia@gmail.com>
2023-09-08 19:53:58 -0500

    rename page

--- src/app/views/download-minutes/download-minutes.page.ts
+++ src/app/views/download-minutes/download-minutes.page.ts
@@ -2,17 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-download-minutas',
-  templateUrl: './download-minutas.page.html',
-  styleUrls: ['./download-minutas.page.scss'],
+  selector: 'app-download-minutes',
+  templateUrl: './download-minutes.page.html',
+  styleUrls: ['./download-minutes.page.scss'],
 })
 export class DownloadMinutesPage implements OnInit {
 
   constructor(private router: Router) { }
 
-  navigateHome(){
-    this.router.navigate(["home"])
-  }
+  // navigateHome(){
+  //   this.router.navigate(["home"])
+  // }
 
   ngOnInit() {
   }
